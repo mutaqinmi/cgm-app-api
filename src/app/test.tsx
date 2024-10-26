89af984c0845a7d56c6daaa775252ada146bf2a2
Eufrat92 <dwirepat@gmail.com>
2024-10-26 15:55:03 +0700

    non editable field

--- src/app/test.tsx
+++ src/app/test.tsx
@@ -1,5 +1,6 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import DrawerMenuItem from "@/components/drawer-menu-item";
 
 export default function Test(){
@@ -19,4 +20,11 @@ export default function Test(){
     // Simpan component disini
     return <IconButton/>
 >>>>>>> ff87fa0 (feat: adding some component)
+=======
+import NonEditedField from "@/components/non-edited-field";
+
+export default function Test(){
+    // Simpan component disini
+    return <NonEditedField/>
+>>>>>>> 7bb80af (non editable field)
 }
