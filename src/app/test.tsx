16e4032fff16af22efd29cba3d093778afb78ba0
Eufrat92 <dwirepat@gmail.com>
2024-10-26 15:55:03 +0700

    non editable field

--- src/app/test.tsx
+++ src/app/test.tsx
@@ -1,6 +1,14 @@
+<<<<<<< HEAD
 import IconButton from "@/components/icon-button";
 
 export default function Test(){
     // Simpan component disini
     return <IconButton/>
+=======
+import NonEditedField from "@/components/non-edited-field";
+
+export default function Test(){
+    // Simpan component disini
+    return <NonEditedField/>
+>>>>>>> 37ab2d3 (non editable field)
 }
