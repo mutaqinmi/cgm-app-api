f8867208395a3887f2c07409ef8d0cae19efe459
mutaqinmi <mutaqinmi2586@gmail.com>
2024-10-26 16:22:00 +0700

    fix: merging branch

--- src/app/test.tsx
+++ src/app/test.tsx
@@ -1,6 +1,14 @@
+<<<<<<< HEAD
 import DrawerMenuItem from "@/components/drawer-menu-item";
 
 export default function Test(){
     // Simpan component disini
     return <DrawerMenuItem/>
+=======
+import Drawer from "@/components/drawer";
+
+export default function Test(){
+    // Simpan component disini
+    return <Drawer/>
+>>>>>>> 12e86a5 (fix: merging branch)
 }
