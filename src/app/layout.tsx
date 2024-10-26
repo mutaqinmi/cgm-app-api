39fd1a1f538f66857a78a4b54eb5973ebb4265bd
zkkaa <muhammadazkafakhri0@gmail.com>
2024-10-26 14:11:00 +0700

    feat: alert popups

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css'  
  
 export default function Layout({ children }: { children: React.ReactNode }) {
-// -  return children
   return (
     <html>
       <body>
--- src/components/alert_popups.tsx
+++ src/components/alert_popups.tsx
@@ -1,20 +1,14 @@
-export default function alert_popups() {
-    return (
-      <div className="w-72 h-48 bg-white border-2 border-black rounded-lg p-5">
-        <div className="mb-5">
-          <span className="text-lg font-semibold">Beralih ke WhatsApp</span>
-        </div>
-        <div className="mb-3">
-          <span className="text-sm">Anda akan dialihkan ke WhatsApp untuk menghubungi Ketua RW</span>
-        </div>
-        <div className="flex justify-between w-full">
-          <button className="w-24 h-9 rounded-md font-medium border-2 border-[#3D8FED] text-[#3D8FED]">
-            <span>Batal</span>
-          </button>
-          <button className="w-24 h-9 rounded-md font-medium bg-[#3D8FED] text-white">
-            <span>Lanjutkan</span>
-          </button>
-        </div>
-      </div>
-    );
-  }
+export default function AlertPopup() {  
+  return<div className="w-72 h-48 bg-white shadow-md rounded-lg p-5">
+    <div className="mb-4">
+      <span className="text-xl font-semibold">Beralih ke WhatsApp</span>
+    </div>
+    <div className="mb-6">
+      <span className="text-sm">Anda akan dialihkan ke WhatsApp untuk menghubungi Ketua RW</span>
+    </div>
+    <div className="flex gap-4 w-full">
+      <button className="w-full h-9 rounded-md font-medium border-2 border-[#3D8FED] text-[#3D8FED]"><span>Batal</span></button>
+      <button className="w-full h-9 rounded-md font-medium bg-[#3D8FED] text-white"><span>Lanjutkan</span></button>
+    </div>
+  </div>
+}
