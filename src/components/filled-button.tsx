0843aeba2f0875c0b0ff14348c5547acb3358c01
zkkaa <muhammadazkafakhri0@gmail.com>
2024-10-26 16:13:22 +0700

    feat: filled button

--- src/components/filled-button.tsx
+++ src/components/filled-button.tsx
@@ -3,5 +3,5 @@ export default function FilledButton() {
         <button className="w-80 h-14 rounded-xl bg-[#3D8FED]">
           <span className="text-white font-medium">Masuk</span>
         </button>
-    );
+    );
   }
