d5334203e5bf2655111f7983b677937413a735c4
Eufrat92 <dwirepat@gmail.com>
2024-10-26 16:43:01 +0700

    tes

--- src/app/test.tsx
+++ src/app/test.tsx
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import IconButton from "@/components/icon-button";
 
 export default function Test(){
@@ -11,4 +12,11 @@ export default function Test(){
     // Simpan component disini
     return <NonEditedField/>
 >>>>>>> 37ab2d3 (non editable field)
+=======
+import PassworInputField from "@/components/password-Input-field";
+
+export default function Test(){
+    // Simpan component disini
+    return <PassworInputField/>
+>>>>>>> 365dfc8 (tes)
 }
