ec1c10f88f1cf5ca51c49da79175723dd7ab27e6
zkkaa <muhammadazkafakhri0@gmail.com>
2024-10-26 16:01:39 +0700

    feat: add filled button

--- src/components/filled-button.tsx
+++ src/components/filled-button.tsx
@@ -3,5 +3,9 @@ export default function FilledButton() {
         <button className="w-80 h-14 rounded-xl bg-[#3D8FED]">
           <span className="text-white font-medium">Masuk</span>
         </button>
+<<<<<<< HEAD
     );
+=======
+    );
+>>>>>>> e580922 (feat: add filled button)
   }
