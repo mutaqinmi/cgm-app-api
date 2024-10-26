12efb580db3b510a174b3783a5db98a42691e8a8
zkkaa <muhammadazkafakhri0@gmail.com>
2024-10-26 15:29:46 +0700

    feat: drawer & drawer menu item

--- src/app/test.tsx
+++ src/app/test.tsx
@@ -1,6 +1,42 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> a0a5b28 (feat: drawer & drawer menu item)
+import DrawerMenuItem from "@/components/drawer-menu-item";
+
+export default function Test(){
+    // Simpan component disini
+    return <DrawerMenuItem/>
+<<<<<<< HEAD
+=======
+import Drawer from "@/components/drawer";
+
+export default function Test(){
+    // Simpan component disini
+    return <Drawer/>
+>>>>>>> 12e86a5 (fix: merging branch)
+=======
+>>>>>>> be82304 (feat: drawer & drawer menu item)
 import IconButton from "@/components/icon-button";
 
 export default function Test(){
     // Simpan component disini
     return <IconButton/>
+<<<<<<< HEAD
+=======
+>>>>>>> ff87fa0 (feat: adding some component)
+=======
+import NonEditedField from "@/components/non-edited-field";
+
+export default function Test(){
+    // Simpan component disini
+    return <NonEditedField/>
+>>>>>>> 7bb80af (non editable field)
+=======
+>>>>>>> a0a5b28 (feat: drawer & drawer menu item)
+>>>>>>> be82304 (feat: drawer & drawer menu item)
 }
--- src/components/drawer-menu-item.tsx
+++ src/components/drawer-menu-item.tsx
@@ -2,6 +2,7 @@
 import { User } from "@phosphor-icons/react";
 
 export default function DrawerMenuItem() {
+<<<<<<< HEAD
   return <div className="flex items-center h-8 cursor-pointer">
     <div>
       <User className="w-6 h-6 mr-6" />
@@ -11,3 +12,16 @@ export default function DrawerMenuItem() {
     </div>
   </div>
 }
+=======
+    return (
+      <div className="flex items-center h-8 cursor-pointer">
+        <div>
+          <User className="w-6 h-6 mr-6" />
+        </div>
+        <div>
+          <span className="text-base">Tentang Saya</span>
+        </div>
+      </div>
+    );
+  }
+>>>>>>> a0a5b28 (feat: drawer & drawer menu item)
