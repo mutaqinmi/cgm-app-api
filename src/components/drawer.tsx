5691a131985b3eb5d7d28e232ec61c22e4a77cea
Eufrat92 <dwirepat@gmail.com>
2024-10-28 11:30:58 +0700

    fix: drawer

--- src/components/drawer.tsx
+++ src/components/drawer.tsx
@@ -1,14 +1,4 @@
 export default function Drawer() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 8cd154d (feat: drawer and drawer menu item)
->>>>>>> 91b03af (feat: drawer and drawer menu item)
-=======
->>>>>>> a3166ca (fix: merging)
     return <div className="w-80 h-screen shadow-md px-8 py-8">
             <div className="w-full h-full">
               <div className="flex items-center w-full mb-12 ml-[-6px]">
@@ -24,32 +14,5 @@ export default function Drawer() {
               </div>
             </div>
           </div>
-        }
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
-  return <div className="w-80 h-screen shadow-md px-8 py-8">
-          <div className="w-full h-full">
-            <div className="flex items-center w-full mb-12 ml-[-6px]">
-              <label>
-                <div className="w-9 h-9 cursor-pointer flex flex-col items-center justify-center">
-                  <input className="hidden peer" type="checkbox" />
-                    <div className="mt-[-3] w-[60%] h-[2px] bg-black rounded-sm transition-all duration-300 origin-left translate-y-[0.45rem] peer-checked:rotate-[-45deg]"></div>
-                    <div className="mt-[-3] w-[60%] h-[2px] bg-black rounded-md transition-all duration-300 origin-center peer-checked:hidden"></div>
-                    <div className="mt-[-3] w-[60%] h-[2px] bg-black rounded-md transition-all duration-300 origin-left -translate-y-[0.45rem] peer-checked:rotate-[45deg]"></div>
-                </div>
-              </label>
-              <div className="ml-6 text-xl font-semibold"><span>Menu</span></div>
-            </div>
-          </div>
-        </div>
       }
 
->>>>>>> a0a5b28 (feat: drawer & drawer menu item)
-=======
-        }
->>>>>>> 8cd154d (feat: drawer and drawer menu item)
->>>>>>> 91b03af (feat: drawer and drawer menu item)
-=======
->>>>>>> a3166ca (fix: merging)
