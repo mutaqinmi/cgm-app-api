a48d59ad784ea55b5b6e422efd29823f24abb4d8
Eufrat92 <dwirepat@gmail.com>
2024-10-28 10:55:38 +0700

    feat: merging

--- src/app/test.tsx
+++ src/app/test.tsx
@@ -1,6 +1,14 @@
+<<<<<<< HEAD
 import TextButton from "@/components/text-button";
 
 export default function Test(){
     // Simpan component disini
     return <TextButton/>
+=======
+import DrawerMenuItem from "@/components/drawer-menu-item";
+
+export default function Test(){
+    // Simpan component disini
+    return <DrawerMenuItem/>
+>>>>>>> a0a5b28 (feat: drawer & drawer menu item)
 }
